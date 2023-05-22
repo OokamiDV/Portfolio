972dfa4ec62885d1e1472164c4508c6da265f06b
OokamiDV <83276647+OokamiDV@users.noreply.github.com>
2023-05-22 12:39:15 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -9,59 +9,57 @@ let certif = document.getElementById('certif');
 
 numTel.hidden = true;
 
-icoButton.onclick = function () {
+icoButton.onclick = function() {
     icoButton.hidden = true;
     numTel.hidden = false;
 };
 
-vkIco.onmouseover = function () {
+vkIco.onmouseover = function() {
     vkIco.style.filter = "invert(1)";
 };
 
-vkIco.onmouseout = function () {
+vkIco.onmouseout = function() {
     vkIco.style.filter = "invert(0)";
 }
 
-TelegaIco.onmouseover = function () {
+TelegaIco.onmouseover = function() {
     TelegaIco.style.filter = "invert(1)";
 };
 
-TelegaIco.onmouseout = function () {
+TelegaIco.onmouseout = function() {
     TelegaIco.style.filter = "invert(0)";
 }
 
-githIco.onmouseover = function () {
+githIco.onmouseover = function() {
     githIco.style.filter = "invert(1)";
 };
 
-githIco.onmouseout = function () {
+githIco.onmouseout = function() {
     githIco.style.filter = "invert(0)";
 }
 
-icoBut.onmouseover = function () {
+icoBut.onmouseover = function() {
     icoBut.style.filter = "invert(1)";
 };
 
-icoBut.onmouseout = function () {
+icoBut.onmouseout = function() {
     icoBut.style.filter = "invert(0)";
 }
 
 
 
-proj.onmouseover = function () {
+proj.onmouseover = function() {
     proj.style.color = "rgb(47, 163, 170)"
 };
 
-proj.onmouseout = function () {
+proj.onmouseout = function() {
     proj.style.color = "rgb(170, 170, 170)"
 };
 
-certif.onmouseover = function () {
+certif.onmouseover = function() {
     certif.style.color = "rgb(47, 163, 170)"
 };
 
-certif.onmouseout = function () {
+certif.onmouseout = function() {
     certif.style.color = "rgb(170, 170, 170)"
 };
-
-
